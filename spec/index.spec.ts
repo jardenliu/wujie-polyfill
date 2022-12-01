ca585dcad9a7c94d27dc8d5548af7ce65acf5c14
jardenliu <15915839707@163.com>
2022-12-01 09:56:14 +0800

    chore: 🧪测试无界加载

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,12 +1,14 @@
 describe(`something`, function () {
     beforeAll(async () => {
-        await page.goto('http://localhost:9876', {
-            timeout: 60000,
-        })
+        await page.goto('http://localhost:9876')
+        await page.waitForSelector('wujie-app')
+        await page.waitFor(2000)
     })
 
-    it('should load without error', async () => {
+    it('should load wujie subapp without error', async () => {
         const title = await page.evaluate(() => document.title)
+        const appContent = await page.evaluate(() => window[0].document.body.innerText)
         expect(title).toContain('wujie-polyfill')
+        expect(appContent.toLowerCase()).toContain('app')
     })
 })
