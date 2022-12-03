c31abe86201331f1f391c06a7cf49148867c3a1b
jardenliu <jarden@qq.com>
2022-12-04 02:14:27 +0800

    chore: 🥰独立核心代码

--- examples/webpack/src/components/pages/main.tsx
+++ examples/webpack/src/components/pages/main.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Wujie from 'wujie-react'
-
+import WujiePolyFill from 'wujie-polyfill'
+console.log(WujiePolyFill)
 export default function App () {
-
     return (<Wujie width={'100vw'} name="app1" height={'100vh'} url={'./app1.html'}></Wujie>)
 }
---
+++ packages/core/rollup.config.ts
@@ -0,0 +1,10 @@
+import ts from '@rollup/plugin-typescript'
+
+export default {
+    input: './src/index.ts',
+    output: {
+        dir: 'dist',
+        format: 'cjs',
+    },
+    plugins: [ts()],
+}
