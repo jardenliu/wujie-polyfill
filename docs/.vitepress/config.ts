676bffc77079d26e6a426c6dff67126c963b280e
jardenliu <15915839707@163.com>
2023-02-01 11:14:33 +0800

    docs: 🚧 初始化文档结构

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,6 +8,101 @@ export default defineConfig({
     vue: {},
     lastUpdated: true,
     themeConfig: {
-        logo: 'https://wujie-micro.github.io/docs/wujie.svg',
+        logo: 'https://wujie-micro.github.io/doc/wujie.svg',
+        socialLinks: [
+            {
+                icon: 'github',
+                link: 'https://github.com/jardenliu/wujie-polyfill',
+            }
+        ],
+        editLink: {
+            pattern: 'https://github.com/jardenliu/wujie-polyfill/tree/main/docs/:path',
+            text: '编辑本页',
+        },
+        footer: {
+            message: 'Released the MIT License.',
+        },
+        nav: [
+            {
+                text: '指南',
+                link: '/guide/',
+                activeMatch: '/guide/',
+            },
+            {
+                text: '插件',
+                link: '/plugins/',
+                activeMatch: '/plugins/',
+            }
+
+        ],
+        sidebar: {
+            '/guide/': [
+                {
+                    text: '入门',
+                    collapsible: true,
+                    items: [
+                        {
+                            text: '介绍',
+                            link: '/guide/',
+                        },
+                        {
+                            text: '快速上手',
+                            link: '/guide/start',
+                        }
+                    ],
+                },
+                {
+                    text: '指南',
+                    collapsible: true,
+                    items: [
+                        {
+                            text: '配置',
+                            link: '/guide/config',
+                        },
+                        {
+                            text: '场景限制',
+                            link: '/guide/limit',
+                        }
+                    ],
+                }
+            ],
+            '/plugins/': [
+                {
+                    text: '插件',
+                    collapsible: true,
+                    items: [
+                        // {
+                        //     text: 'LocationSyncPlugin',
+                        //     link: '/plugins/locationSync',
+                        // },
+                        {
+                            text: 'LocationReloadPlugin',
+                            link: '/plugins/locationReload',
+                        },
+                        {
+                            text: 'EventTargetPlugin',
+                            link: '/plugins/eventTarget',
+                        },
+                        {
+                            text: 'WindowGetterPlugin',
+                            link: '/plugins/windowGetter',
+                        },
+                        {
+                            text: 'WindowMessagePlugin',
+                            link: '/plugins/windowMessage',
+                        },
+                        {
+                            text: 'DocFullScrollPlugin',
+                            link: '/plugins/docFullScroll',
+                        },
+                        {
+                            text: 'InstanceofPlugin',
+                            link: '/plugins/instanceof',
+                        }
+
+                    ],
+                }
+            ],
+        },
     },
 })
