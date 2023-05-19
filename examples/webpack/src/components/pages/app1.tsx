ad9945d3150eccce8d1c20fe4d9f044edfac2526
jardenliu <15915839707@163.com>
2023-05-19 10:31:34 +0800

    refactor: 🏢 polyfillEventPath

--- examples/webpack/src/components/pages/app1.tsx
+++ examples/webpack/src/components/pages/app1.tsx
@@ -2,7 +2,32 @@ import React, { useState } from 'react'
 
 export default function App () {
     const [msg, setMsg] = useState('App1')
-    console.log('window2 :', window.window.location)
+    const input2 = document.createElement('input')
+    input2.id = 'input'
+    document.body.appendChild(input2)
+    setTimeout(() => {
+        const input = document.querySelector('#input')
+        // input.addEventListener('input', (event) => {
+        //     setTimeout(() => {
+        //         console.log(1, event.target)
+        //     }, 1000)
+        // })
+
+        input.oninput = function (event) {
+            console.log(3, event.target)
+            setTimeout(() => {
+                console.log(2, event.target)
+            }, 1000)
+        }
+
+        // window.addEventListener('keydown', (event) => {
+        //     setTimeout(() => {
+        //         console.log(2, event.srcElement)
+        //     }, 1000)
+        // } )
+
+        console.log('input :', input)
+    }, 2000)
     return (<div>
         <input></input>
     </div>)
